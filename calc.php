9d9946abca69e8234cb0bbfab2a32da59d4a383b
vkopysov <vk.nure@gmail.com>
2016-11-23 15:52:31 +0200

    new class MyException

--- calc.php
+++ calc.php
@@ -8,30 +8,40 @@ error_reporting(E_ALL);
 //Fatal: E_PARSE E_ERROR
 //Non-Fatal E_WARNING, E_NOTICE E_DEPRECATED E_STRICT
 
-
  
     try{
-        echo calculate('asd',2);
+        echo calculate('ex',2);
         echo '<br>';
         // echo calculate(1,0);
         echo '<br>';
         //echo calculate(10,5);
         echo '<br>';
     } catch (Exception $ex) {
-           echo $ex->getMessage();
+           echo $ex->getMessage()." Код ошибки : ".$ex->getCode();
+           
     }
     
     
     
 function calculate($num1, $num2){
-   if(!is_numeric($num1)){
-        throw new Exception("Первый аргумент не число");
+    
+    if($num1="ex"){
+         throw new MyException("Получена моя ошибка!");
+    }
+    
+    if(!is_numeric($num1)){
+        throw new Exception("Первый аргумент не число!");
     }
     if(!is_numeric($num2)){
-        throw new Exception("Второй аргумент не число");
+        throw new Exception("Второй аргумент не число!");
     }
     if($num2 == 0){
-        throw new Exception("Второй аргумент 0");
+        throw new Exception("Второй аргумент 0!");
     }
    return $num1/$num2;
+}
+
+class MyException extends Exception{
+    protected $code = 88005553535;
+
 }
